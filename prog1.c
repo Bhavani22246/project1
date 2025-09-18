c9d9df0df8ac448d9e8bceeb7a2922ccaa5e5055
Bhavani22246 <kulkarnibhavani7@gmail.com>
2025-09-18 09:20:55 +0530

    Update prog1.c

--- prog1.c
+++ prog1.c
@@ -1,5 +1,6 @@
-this is my program 1
+this is my program 
 this is my second statement of the file
 this line i have written from feature branch
 this line i am going to write again in the feature branch
 this is an git hub project sample
+iam editing my git hub project now
